fb6faa1bd24929b171dcaaa1d32969b202cd4570
ArcticLuma113 <aplumafreak500@gmail.com>
2024-02-19 14:32:30 -0500

    Update copyright dates

--- include/kcpsession.h
+++ include/kcpsession.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: AGPL-3.0-or-later */
 /* This file is part of yagips.
 
-©2023 Alex Pensinger (ArcticLuma113)
+©2024 Alex Pensinger (ArcticLuma113)
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
