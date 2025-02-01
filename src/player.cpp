79a2dd62da9733fe49fd29647dadf60e087deb2a
ArcticLuma113 <aplumafreak500@gmail.com>
2025-02-01 16:53:59 -0500

    Set world level in EnterSceneNotify packet

--- src/player.cpp
+++ src/player.cpp
@@ -665,6 +665,7 @@ void Player::onLogin(Session& s) {
 	esn.set_target_uid(uid);
 	esn.set_enter_scene_token(tpToken);
 	esn.set_is_first_login_enter_scene(1);
+	esn.set_world_level(worldLevel);
 	esn.set_enter_reason(1);
 	esn.set_type(proto::ENTER_SELF);
 	if (esn.SerializeToString(&pkt_data)) {
