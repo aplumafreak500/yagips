0ef29829dfe3fd021268bc656675ed0557671057
ArcticLuma113 <aplumafreak500@gmail.com>
2025-02-18 11:55:56 -0500

    Implement GetPlayerBlacklistReq

--- src/packet_handlers.cpp
+++ src/packet_handlers.cpp
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: AGPL-3.0-or-later */
 /* This file is part of yagips.
 
-©2024 Alex Pensinger (ArcticLuma113)
+©2025 Alex Pensinger (ArcticLuma113)
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
@@ -19,6 +19,7 @@ extern int handlePingReq(Session&, std::string&, std::string&);
 extern int handleGetPlayerTokenReq(Session&, std::string&, std::string&);
 extern int handlePlayerLoginReq(Session&, std::string&, std::string&);
 extern int handleEnterSceneReadyReq(Session&, std::string&, std::string&);
+extern int handleGetPlayerBlacklistReq(Session&, std::string&, std::string&);
 
 int processPacket(Session& session, Packet& packet, int* isValid) {
 	unsigned int opcode = packet.getOpcode();
@@ -38,5 +39,7 @@ int processPacket(Session& session, Packet& packet, int* isValid) {
 		return handlePlayerLoginReq(session, header, data);
 	case 282:
 		return handleEnterSceneReadyReq(session, header, data);
+	case 4039:
+		return handleGetPlayerBlacklistReq(session, header, data);
 	}
 }
---
+++ src/packets/player_blacklist.cpp
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: AGPL-3.0-or-later */
+/* This file is part of yagips.
+
+©2025 Alex Pensinger (ArcticLuma113)
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. */
+
+#include <stdio.h>
+#include <string>
+#include "packet.h"
+#include "session.h"
+#include "social.pb.h"
+
+int handleGetPlayerBlacklistReq(Session& session, std::string&, std::string& data) {
+	proto::GetPlayerBlacklistRsp rsp;
+	Packet rsp_pkt(4040);
+	rsp.set_retcode(0);
+	// TODO Implement
+	if (!rsp.SerializeToString(&data)) {
+		fprintf(stderr, "Error building packet data\n");
+		return -1;
+	}
+	rsp_pkt.buildHeader(2);
+	rsp_pkt.setData(data);
+	return session.sendPacket(rsp_pkt);
+}
